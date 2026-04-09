32dd442794d6aa31eea0c430f5756c8fab3e2633
Abi Chown <abi.chown7@gmail.com>
2026-04-09 17:32:23 +0100

    Move /stats endpoint

--- src/app/api/stats/route.ts
+++ src/app/api/stats/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../../../generated/prisma/client";
+import { PrismaClient } from "../../../generated/prisma/client";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
